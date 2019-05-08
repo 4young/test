8eae4d40189a3ee9485a33187b4435e9dc1bdf6b
4young <a1636762971@qq.com>
2019-05-07 17:37:23 -0700

    Add files via upload

---
+++ timeout.py
@@ -0,0 +1,11 @@
+import requests
+from requests.exceptions import RequestException
+target = "http://219.166.7.50"
+port = "80"
+print("%s:%s"%(target,port))
+try:
+  res = requests.get(target + ':' + port, timeout = 5)
+  print(res.text)
+except RequestException:
+  print("[!]RequestException:timeout")
+    
